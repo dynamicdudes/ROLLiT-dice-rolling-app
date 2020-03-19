d6e8784364f3355412c55d64da0dc8c02303af8b
Vishweshwaran <vishwa300499@gmail.com>
2020-03-19 23:49:58 +0530

    Initial Stage

---
+++ app/src/main/java/com/dynamicdudes/rollit/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.dynamicdudes.rollit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    var randomNumber1 : Int = 0
+    var randomNumber2 : Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val actionBar = supportActionBar
+        actionBar?.title = "The dice of Zeus always fall luckily."
+        roll_button_clicked.setOnClickListener {
+            rollDice()
+        }
+    }
+    fun rollDice(){
+        randomNumber1 = selectImage(Random.nextInt(6) + 1)
+        randomNumber2 = selectImage(Random.nextInt(6) + 1)
+        dice_image_1.setImageResource(randomNumber1)
+        dice_image_2.setImageResource(randomNumber2)
+    }
+    fun selectImage(ranNum:Int) : Int{
+        return when(ranNum){
+            1 -> R.drawable.dice1
+            2 -> R.drawable.dice2
+            3 -> R.drawable.dice3
+            4 -> R.drawable.dice4
+            5 -> R.drawable.dice5
+            else -> R.drawable.dice6
+        }
+    }
+}
